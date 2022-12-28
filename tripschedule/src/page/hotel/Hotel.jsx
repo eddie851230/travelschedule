383a73df73d53494df99b8a54d85ef206707a434
Chia Cheng Weng <75014019+eddie851230@users.noreply.github.com>
2022-12-28 17:17:27 +0800

    '12/28hotel圖片置入'

--- tripschedule/src/page/hotel/Hotel.jsx
+++ tripschedule/src/page/hotel/Hotel.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import "../hotel/index.css"
+import "../hotel/hotel-index.css"
+
 class Hotel extends Component {
     state = {  } 
     render() { 
-        return (<h1>Hotel</h1>);
-    }
+        return ( 
+            <div id="contextBG"></div>
+        )
+}
 }
  
 export default Hotel;
