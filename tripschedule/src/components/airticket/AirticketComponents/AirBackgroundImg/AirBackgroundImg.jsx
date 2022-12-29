fcfebdf924e6f0abd20273fc44424e115085b3f0
leogitD <leohubD>
2022-12-29 15:31:08 +0800

    Airticket react

---
+++ tripschedule/src/components/airticket/AirticketComponents/AirBackgroundImg/AirBackgroundImg.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./AirBackgroundImg.css"
+
+function AirBackgroundImg() {
+        return (<div id='AirticketBgImg' className={this.props.className}></div>);
+}
+ 
+export default AirBackgroundImg;
