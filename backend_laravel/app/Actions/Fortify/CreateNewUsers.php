03d60f6eb66283693f11f70f481dcf11f38a13d0
Eddie Weng <jia.chun.weng@gmail.com>
2023-01-21 15:33:38 +0800

    合併

---
+++ backend_laravel/app/Actions/Fortify/CreateNewUsers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateNewUser implements CreatesNewUsers
+{
+  use PasswordValidationRules;
+  /**
+   * Validate and create a newly registered user.
+   *
+   * @param  array<string, string>  $input
+   */
+  public function create(array $input): User
+  {
+    Validator::make($input, [
+      'name' => ['required', 'string', 'max:255'],
+      'email' => [
+        'required',
+        'string',
+        'email',
+        'max:255',
+        Rule::unique(User::class),
+      ],
+      'password' => $this->passwordRules(),
+    ])->validate();
+    return User::create([
+      'name' => $input['name'],
+      'email' => $input['email'],
+      'password' => Hash::make($input['password']),
+      'profile_photo_path' => '/img/memberimgAfterlogin.webp',
+      'coverphoto_path' => 'https://upload.wikimedia.org/wikipedia/commons/3/36/Lake_Kawaguchiko_Sakura_Mount_Fuji_4.JPG'
+    ]);
+  }
+}
---
+++ backend_laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Support\Facades\DB;
+use Laravel\Fortify\Fortify;
+
+class AuthController extends Controller
+{
+
+  //
+  public function register(Request $request)
+  {
+    $user = (new CreateNewUser)->create($request->all());
+
+    $token = $user->createToken('token')->plainTextToken;
+    return [
+      'user' => $user,
+      'token' => $token
+    ];
+  }
+
+  public function login(Request $request)
+  {
+    $validated = $request->validate([
+      'email' => 'required|email',
+      'password' => 'required|string|min:8'
+    ]);
+    $user = User::where('email', $validated['email'])->first();
+
+    if (!$user || !Hash::check($validated['password'], $user['password'])) {
+      return response([
+        'message' => '帳密錯誤'
+      ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    $token = $user->createToken('apiToken')->plainTextToken;
+    return response([
+      'user' => $user,
+      'token' => $token
+    ], Response::HTTP_CREATED);
+  }
+  public function logout()
+  {
+    Auth::user()->tokens()->delete();
+
+    return  response([], Response::HTTP_OK);
+  }
+}
